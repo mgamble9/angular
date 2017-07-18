27a86e217d71de4681b958b209406585eff22469
Mike Gamble <mgamble9@yahoo.com>
2017-07-17 17:22:55 -0700

    Power Levels Assignment (parent/child components and using inputs into children

---
+++ Power-Levels/src/app/power/saiyan/saiyan.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-saiyan',
+  templateUrl: './saiyan.component.html',
+  styleUrls: ['./saiyan.component.css']
+})
+export class SaiyanComponent implements OnInit {
+  @Input() power;
+
+  constructor() { }
+
+  ngOnInit() {
+      this.power = this.power *10;
+  }
+
+  // ngOnChanges() {
+  //   this.power = this.power * 10;
+  // }
+
+}

f50b3876e108cbfed3eb285fe1310482e845a6af
Mike Gamble <mgamble9@yahoo.com>
2017-07-19 18:49:27 -0700

    Dojo Weather Forecast (using a service from API and routing)

---
+++ Dojo-Weather-Forecast/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BurbankComponent } from './burbank/burbank.component';
+import { ChicagoComponent } from './chicago/chicago.component';
+import { DallasComponent } from './dallas/dallas.component';
+import { DcComponent } from './dc/dc.component';
+import { LandingComponent } from './landing/landing.component';
+import { SanjoseComponent } from './sanjose/sanjose.component';
+import { SeattleComponent } from './seattle/seattle.component';
+
+const routes: Routes = [
+  { path: '', component: LandingComponent , pathMatch: 'full' },
+  { path: 'seattle', component: SeattleComponent },
+  { path: 'sanjose', component: SanjoseComponent },
+  { path: 'burbank', component: BurbankComponent },
+  { path: 'dallas', component: DallasComponent },
+  { path: 'dc', component: DcComponent },
+  { path: 'chicago', component: ChicagoComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
